21fe0b446f2b521a5748a435d5725b434e6e9ba7
Reza <reza.armiun@yahoo.com>
2019-11-02 23:34:37 +0330

    creating recipe project with spring initializer.

--- src/main/java/raze/springboot/recipeapp/RecipeAppApplication.java
+++ src/main/java/raze/springboot/recipeapp/RecipeAppApplication.java
@@ -2,12 +2,14 @@ package raze.springboot.recipeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+
 public class RecipeAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RecipeAppApplication.class, args);
+        ApplicationContext ctx=SpringApplication.run(RecipeAppApplication.class, args);
     }
 
 }
