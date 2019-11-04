57c1124e20d6a96621daa4850ed8f0744cd8f77f
Reza <reza.armiun@yahoo.com>
2019-11-04 23:40:30 +0330

    adding  data.sql to resources and modified GenerationType.Auto to GenerationType.IDENTITY .Closes #5

--- src/main/java/raze/springboot/recipeapp/model/UnitOfMeasure.java
+++ src/main/java/raze/springboot/recipeapp/model/UnitOfMeasure.java
@@ -1,14 +1,16 @@
 package raze.springboot.recipeapp.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Entity
 public class UnitOfMeasure {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
     private String description;
+
 
     public Long getId() {
         return id;
