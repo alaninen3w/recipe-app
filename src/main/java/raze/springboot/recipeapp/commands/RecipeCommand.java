8001896fd14b38c4239795f79906b3c39f2f9f7c
Reza <reza.armiun@yahoo.com>
2019-11-08 20:38:20 +0330

    modifying RecipeCommand attributes . Closes #21

--- src/main/java/raze/springboot/recipeapp/commands/RecipeCommand.java
+++ src/main/java/raze/springboot/recipeapp/commands/RecipeCommand.java
@@ -1,28 +1,29 @@
-package raze.springboot.recipeapp.model.commands;
+package raze.springboot.recipeapp.commands;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import raze.springboot.recipeapp.model.Category;
 import raze.springboot.recipeapp.model.Difficulty;
-import raze.springboot.recipeapp.model.Ingredient;
 
 import java.util.HashSet;
 import java.util.Set;
 @Getter
 @Setter
+@Builder
 public class RecipeCommand {
     private Long id;
+    private String description;
     private Integer prepTime;
     private Integer cookTime;
     private Integer serving;
     private String source;
     private String url;
     private String directions;
-    private Set<Ingredient> ingredients=new HashSet<>();
+    private Set<IngredientCommand> ingredients=new HashSet<>();
     private Byte[] image;
     private Difficulty difficulty;
     private NotesCommand notes;
-    private Set<Category> categories=new HashSet<>();
+    private Set<CategoryCommand> categories=new HashSet<>();
 
 
 
